89919ae993a90f30fbb1a836ab6e8ec1d6f3a010
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-07-29 22:42:34 -0300

    Versão Beta – 2025-07-23

--- modules/Shopping/routes/web.php
+++ modules/Shopping/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Shopping\App\Livewire\Page\Cart;
-use Modules\Shopping\App\Livewire\Page\checkout;
+use Modules\Shopping\App\Livewire\Page\Checkout;
 use Modules\Shopping\App\Livewire\Page\Payment;
 
 // Rotas do módulo Shopping
@@ -15,6 +15,6 @@ Route::middleware(['web'])->group(function () {
     Route::get('/carrinho', Cart::class)
         ->name('cart');
 
-    Route::get('/cliente', checkout::class)
+    Route::get('/cliente', Checkout::class)
         ->name('checkout');
 });
--- resources/views/components/layouts/admin/application-logo.blade.php
+++ resources/views/components/layouts/admin/application-logo.blade.php
@@ -1,16 +1,19 @@
 <div>
-    @props(['width' => 'h-24 sm:h-16'])
-    @if (Storage::directoryMissing('public/logos-system'))
-        <picture>
-            <source srcset="{{ url('storage/logos/logo-system.png') }}" />
-            <source srcset="{{ url('storage/logos/logo-system.webp') }}" />
-            <img class="{{ $width }}" src="{{ url('storage/logos/logo-system.png') }}" alt="api-system">
-        </picture>
-    @else
+    <span class="flex h-20 w-20 items-center justify-center rounded-md">
+        <x-app-logo-icon class="me-2 h-7 fill-current text-white" />
+    </span>
+    {{-- @props(['width' => 'h-24 sm:h-16'])
+    @if (Storage::directoryMissing('public/logos-system')) --}}
+    {{-- <picture>
+        <source srcset="{{ url('storage/logos/logo-system.png') }}" />
+        <source srcset="{{ url('storage/logos/logo-system.webp') }}" />
+        <img class="{{ $width }}" src="{{ url('storage/logos/logo-system.png') }}" alt="api-system">
+    </picture> --}}
+    {{-- @else
         <picture class="{{ $width }}">
             <source srcset="{{ url('storage/logos-system/logo.png') }}" />
             <source srcset="{{ url('storage/logos-system/logo.webp') }}" />
             <img class="{{ $width }}" src="{{ url('storage/logos-system/logo.png') }}" alt="api-system">
         </picture>
-    @endif
+    @endif --}}
 </div>
