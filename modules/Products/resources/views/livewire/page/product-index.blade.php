d6a9c53e99b3b3d9f772dd49d6db3c28e32d4151
Osvaldo Laini <osvaldolaini@hotmail.com>
2025-07-31 16:42:25 -0300

    Versão Beta – 2025-07-23

--- modules/Products/resources/views/livewire/page/product-index.blade.php
+++ modules/Products/resources/views/livewire/page/product-index.blade.php
@@ -28,7 +28,7 @@
             <button wire:click='changeStore({{ $item->id }})'>
                 @if ($item->logo_path)
                     {{-- <div class="avatar"> --}}
-                    <div class="relative w-8 rounded-full cursor-pointer">
+                    <div class="relative w-12 rounded-full cursor-pointer">
                         <!-- Avatar pequeno -->
                         <img src="{{ url('storage/stores/' . $item->id . '/' . $item->code_image . '_list.png') }}"
                             alt="{{ $item->title }}">
@@ -36,7 +36,7 @@
                     {{-- </div> --}}
                 @else
                     {{-- <div class="avatar"> --}}
-                    <div class="relative w-8 rounded-full cursor-pointer">
+                    <div class="relative w-12 rounded-full cursor-pointer">
                         <x-layouts.admin.application-logo width="h-12"></x-layouts.admin.application-logo>
                     </div>
                     {{-- </div> --}}
